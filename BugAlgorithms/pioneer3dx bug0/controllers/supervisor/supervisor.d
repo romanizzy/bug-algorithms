08a2eaa2414339d0418237af9a8c162cc9b10347
Izabella Valero <109047364+romanizzy@users.noreply.github.com>
2024-07-22 10:48:59 -0500

    Bug2 Algorithm with E-Puck in Webots

--- BugAlgorithms/pioneer3dx bug0/controllers/supervisor/supervisor.d
+++
@@ -1,5 +0,0 @@
-supervisor.o: supervisor.c \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/supervisor.h \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/nodes.h
