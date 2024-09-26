825320e520879be5c4ce5aa672616747d2476615
Izabella Valero <109047364+romanizzy@users.noreply.github.com>
2024-09-26 18:02:52 -0500

    Update to turn and end styles of bug 2

--- bug2/bug2.py
+++ bug2/bug2.py
@@ -1,49 +1,88 @@
-# important points:
-# use <robot_position> to get current position of robot in <x,y,theta> format.
-# use <robot_omega> to get current values for the wheels in <w1,w2,w3> format.
+"""sensor_rewrite controller."""
 
 import numpy as np
 from initialization import *
 import math
 
-# boolean function: Checks ifrobot is on m_line
-def is_on_M_line(x, y, threshold=0.1):
-    goal_point_X = 0.25
-    # print(abs(x - goal_point_X))
-    # print("On M_Line: ", abs(x - goal_point_X) < threshold)
-    return abs(x-goal_point_X) < threshold
+
+def calculate_target_angle(robot_pos, goal_pos):
+    #the goal will be calculated based on 0 degrees started at north going clockwise
+    #the robot will be calculated based on 0 degrees started at east going counter clockwise
+    # this function calculates the goal angle and then normalizes it on a 0-360 degrees
+    # then it will negate the angle so it will be based on the counter clockwise
+    # then add 90 to start 0 degrees from east, and add 360 to get an angle betweeen 0-360
+    dx = goal_pos[0] - robot_pos[0]
+    dy = goal_pos[1] - robot_pos[1]
+    angle_to_target = math.degrees(math.atan2(dx, dy))
+    #print("raw angle: ", angle_to_target)
+    if angle_to_target < 0:
+        angle_to_target += 360
+    elif angle_to_target > 360:
+        angle_to_target -= 360
+    angle_to_target *= -1
+    angle_to_target += 450
+    #print("normalised angle: ", angle_to_target)
+    return angle_to_target
+
 
 # boolean function: rotates robot if not pointed to M
-def align_to_M(heading, theta, threshold, turn_left=False):
+def align_to_M(target_angle, yaw_angle, threshold = 3):
     ts = 1  # turning speed
-    print("Values: ", heading, theta, turn_left)
-    print("Difference: ", heading-theta)
-    print("Absolute Value: ",abs(heading - theta) < threshold)
-    if turn_left:
-        ts *= -1  # turn left
-    if abs(heading - theta) < threshold:
+    turn = 'left'
+    difference = target_angle - yaw_angle
+    print("yaw angle: ", yaw_angle)
+    print("Target Angle: ", target_angle)
+    print("Difference", difference)
+
+
+    if(difference < 0):
+        turn = 'right'
+
+    if  abs(difference) < threshold:
+        print("true")
         return True
     else:
-        
-        update_motor_speed(input_omega=[ts/20, -1*ts])
+        if turn == 'right':
+            update_motor_speed(input_omega=[ts/20, -ts])
+            print("right turning")
+        else:
+            update_motor_speed(input_omega=[-ts, ts/20])
+            print("left turning")
         return False
 
 # returns distance between two given points
 def calculate_euclidean_distance(x1, y1, x2, y2):
-    print("Euclidean Distance: ", math.sqrt((x1-x2)**2 + (y1-y2)**2))
+    # print("Euclidean Distance: ", math.sqrt((x1-x2)**2 + (y1-y2)**2))
     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+# returns slope of the m-line between goal and start
+def calculate_slope(x1, y1, x2, y2):
+    # print("Y: ", (y1-y2))
+    # print("X: ", (x1-x2))
+    if (x1-x2) == 0:
+        return x1
+    print("Slope: ", (y1-y2)/(x1-x2))
+    return((y1-y2)/(x1-x2))
+
+# boolean function: Checks if robot is on m_line
+# write in the m-line to compare instead of calculating the slope every time
+def is_on_M_line(currX, currY, goalX, goalY, m_line, threshold=1):
+    slope = calculate_slope(currX, currY, goalX, goalY)
+    # print("Slope: ", slope)
+    # print("m_line: ", m_line)
+    # print("Difference: ", m_line - slope)
+    return abs(m_line - slope) < threshold
 
 if __name__ == "__main__":
     # initialization of robot
     TIME_STEP = 32
-    robot = init_robot(time_step=TIME_STEP)
+    robot, goal_pos, start_pos = init_robot(time_step=TIME_STEP)
     print("Robot Initialized")
     init_robot_state(in_pos=[0,0,0], in_omega=[0,0])
     prev = ""
-    
-    # try getting position of another object or goal object instead of hardcode
-    goal_position = 0.25, -1.5
 
+    # calculate m-line
+    m_line = calculate_slope(goal_pos[0], goal_pos[1], start_pos[0], start_pos[1])
     # define local variables
     state = 'start'
     robot_speed = 3
@@ -57,12 +96,12 @@ if __name__ == "__main__":
     while robot.step(TIME_STEP) != -1:
 
         # trash variable for unused sonar. can be taken out of initialization.py
-        gps_values, compass_val, trash, encoder_value, ir_value = read_sensors_values()
-        # print("Sensor Read Complete")
+        gps_values, compass_val, encoder_value, ir_value, imu_yaw = read_sensors_values()
+        #print("Sensor Read Complete")
+        # print("GPS Values: ", gps_values[0], gps_values[1])
         front_ir_values = ir_value[0], ir_value[7]
         right_ir_values = ir_value[1], ir_value[2]
         left_ir_values = ir_value[5], ir_value[6]
-
 
         update_robot_state()
         # print("Current: ", state)
@@ -72,29 +111,31 @@ if __name__ == "__main__":
         # calculate and save m-line points instead.
         # function also works when if statement is taken out.
         if state == 'start':
-            # print("Running start state")
-            if(is_on_M_line(gps_values[0],gps_values[1])):
-                prev = state 
-                state = 'align_robot_heading'
+            print("Running start state")
+            #if(is_on_M_line(gps_values[0], gps_values[1], goal_pos[0], goal_pos[1], m_line)):
+            prev = state 
+            state = 'align_robot_heading'
+            
 
         # checks to see if robot is aligned. if align, start moving
         elif state == 'align_robot_heading':
-            # print("Running align robot heading state")
-            is_aligned = align_to_M(get_bearing_in_degrees(compass_val), theta=180, threshold=0.1) 
+            print("Running align robot heading state")
+            is_aligned = align_to_M(calculate_target_angle(gps_values, goal_pos), imu_yaw)#need to set the goal here for the object to orient itself to 
             if is_aligned: 
                 prev = state
                 state = 'move_to_goal'
+            calculate_slope(goal_pos[0], goal_pos[1], gps_values[0], gps_values[1])
 
         # updates robot position, moving along the m-line
         # if at goal, stop; if front finds obstacle, wall follow + save hit points
         # maybe calculate slope for m-line and match for alignment and movement?
         elif state == 'move_to_goal':
-            # print("Running move to goal state")
+            print("Running move to goal state")
             update_motor_speed(input_omega=[robot_speed, robot_speed])
             difference = abs(front_ir_values[1] - front_ir_values[0])
             # print("Difference: ", difference)
             # print("Front_ir_values: ", front_ir_values[0], " ", front_ir_values[1])
-            if ((gps_values[0], gps_values[1], goal_position[0], goal_position[1])<0.5):
+            if (calculate_euclidean_distance(gps_values[0], gps_values[1], goal_pos[0], goal_pos[1])<0.17):
                 state = 'end'
             elif (front_ir_values[0] + front_ir_values[1]) / 2 > 800:
                 prev = state
@@ -104,7 +145,8 @@ if __name__ == "__main__":
             
         # follows perimeter of obstacle.
         elif state == 'wall_following':
-            # print("Running wall_following state")
+            print("Running wall_following state")
+            # print(prev)
             left_wall = left_ir_values[0] > 80
             front_wall = ((front_ir_values[0] + front_ir_values[1]) / 2) > 80
             right_wall = right_ir_values[1] > 80
@@ -117,43 +159,41 @@ if __name__ == "__main__":
 
             # found wall in front, default turn to left
             if front_wall: 
-                # print("Running front wall")
+                print("Running front wall")
                 update_motor_speed(input_omega=[-1*robot_speed, robot_speed])
 
             # is on m-line after wall following, creates leave point and aligns to goal again
-            elif (is_on_M_line(gps_values[0], gps_values[1])) and prev == 'wall_following':
+            elif (is_on_M_line(gps_values[0], gps_values[1], goal_pos[0], goal_pos[1], m_line)) and prev == 'wall_following':
                 # print("Running M_line")
                 # print(calculate_euclidean_distance(gps_values[0], gps_values[1], goal_postition[0], goal_postition[1]) < calculate_euclidean_distance(hit_point[-1][0], hit_point[-1][1], goal_postition[0], goal_postition[1]))
-                if calculate_euclidean_distance(gps_values[0], gps_values[1], goal_position[0], goal_position[1]) < calculate_euclidean_distance(hit_point[-1][0], hit_point[-1][1], goal_position[0], goal_position[1]):
-                    #print("Running E-Distance")
+                if calculate_euclidean_distance(gps_values[0], gps_values[1], goal_pos[0], goal_pos[1]) < calculate_euclidean_distance(hit_point[-1][0], hit_point[-1][1], goal_pos[0], goal_pos[1]):
+                    # print("Running E-Distance")
                     leave_point.append([gps_values[0], gps_values[1]])
                     prev = state
                     state = 'align_robot_heading'
 
             # following wall on the right
-            elif right_wall: 
+            elif right_wall:
+                print("Running Right Wall") 
                 update_motor_speed(input_omega=[robot_speed, robot_speed])
-                if calculate_euclidean_distance(gps_values[0], gps_values[1], hit_point[-1][0], hit_point[-1][0]) > 1:
-                    #print("Prev Changed 1")
-                    prev = state
+                #if calculate_euclidean_distance(gps_values[0], gps_values[1], hit_point[-1][0], hit_point[-1][0]) > 0.7:
+                #    print("Prev Changed 1")
+                #    prev = state
 
             # if too far from the wall
             else:
-                #print("Running else")
-                update_motor_speed(input_omega=[robot_speed, robot_speed/4])
-                #print("Prev Changed 2")
-                #prev = state
+                print("Running else")
+                update_motor_speed(input_omega=[robot_speed, robot_speed/2])
+                # print("Prev Changed 2")
+                prev = state
 
         elif state == 'end':
             print("Running end state")
-            is_aligned = align_to_M(get_bearing_in_degrees(compass_val), theta=90, threshold = 0.1)
-            if is_aligned:
-                update_motor_speed(input_omega=[0, 0, 0]) #end
+            update_motor_speed(input_omega=[0, 0, 0]) #end
                 
-        elif(calculate_euclidean_distance(gps_values[0], gps_values[1], goal_position[0], goal_position[1])< 0.1):
+        elif(calculate_euclidean_distance(gps_values[0], gps_values[1], goal_pos[0], goal_pos[1])< 0.1):
             state = 'end'
-            #print("State changed to end")
-            update_motor_speed(input_omega=[0, 0, 0])
         
     pass
-           
+            
+                    
--- bug2/initialization.py
+++ bug2/initialization.py
@@ -1,10 +1,12 @@
 
-from controller import Robot
 from controller import GPS
 from controller import DistanceSensor
 from controller import PositionSensor
 from controller import LidarPoint
 from controller import Display
+from controller import Node
+from controller import Robot
+from controller import Supervisor
 import math
 import numpy as np
 
@@ -14,14 +16,14 @@ CHASSIS_AXLE_LENGTH = 0.22
 wheel_cirum = 2 * math.pi * WHEEL_RADIUS
 encoder_unit = wheel_cirum / (2*math.pi)
 
+goal_block = None
+goal_pos = None
+start_pos = None
 gps = None
 compass = None
 motor_1 = None
 motor_2 = None
 # motor_3 = None
-sonar_1 = None
-sonar_2 = None
-sonar_3 = None
 pos_1 = None
 pos_2 = None
 # pos_3 = None
@@ -35,26 +37,27 @@ ir_7 = None
 ir_8 = None
 gps_values = None
 compass_val = None
-sonar_value = None
 position_value = None
 ir_value = None
+imu_yaw = None
 robot_position = np.array([0.25, 0.0, 0.0])
 robot_omega = np.array([0.0, 0.0])
 
 
 def init_robot(time_step=32):
 
+    global robot
+    global goal_block
+    global goal_pos
+    global start_pos
     global gps
     global compass
+    global imu
     global motor_1
     global motor_2
-    # global motor_3
-    global sonar_1
-    global sonar_2
-    global sonar_3
     global pos_1
     global pos_2
-    # global pos_3
+
     global ir_1
     global ir_2
     global ir_3
@@ -65,14 +68,29 @@ def init_robot(time_step=32):
     global ir_8
 
     # create the Robot instance.
-    robot = Robot()
+    robot = Supervisor()
     
     # get the time step of the current world.
     TIME_STEP = 32
 
+    # get the node for the goal node
+    goal_block = robot.getFromDef('goal')
+    # print("goal block: ", goal_block)
+
+    # Get the position field from the node
+    goal_field = goal_block.getField('translation')
+    start_field = robot.getFromDef('bug').getField('translation')
+
+    # Get the start position and goal position
+    goal_pos = goal_field.getSFVec3f()
+    start_pos = start_field.getSFVec3f()
+
+    # print("Goal pos: ", goal_pos)
+    # print("Start pos: ", start_pos)
+
     # define robot motors
-    motor_1 = robot.getDevice("left wheel motor")
-    motor_2 = robot.getDevice("right wheel motor")
+    motor_1 = robot.getDevice("left_wheel_motor")
+    motor_2 = robot.getDevice("right_wheel_motor")
     # motor_3 = robot.getDevice("motor_3")  
 
     # set position for robot motors
@@ -85,30 +103,20 @@ def init_robot(time_step=32):
     motor_2.setVelocity(0.0)
     # motor_3.setVelocity(0.0)
     
-    print("Motor 1 Velocity ", motor_1.getVelocity())
-    print("Motor 2 Velocity ", motor_2.getVelocity())
+    # print("Motor 1 Velocity ", motor_1.getVelocity())
+    # print("Motor 2 Velocity ", motor_2.getVelocity())
 
-    # define distance sensors
-    sonar_1 = robot.getDevice("s2")
-    sonar_2 = robot.getDevice("s3")
-    sonar_3 = robot.getDevice("s5")
-
-    # enable distance sensors
-    sonar_1.enable(time_step)
-    sonar_2.enable(time_step)
-    sonar_3.enable(time_step)
-    print("Sonar Sensors ", sonar_1, sonar_2, sonar_3)
 
     # define position sensors
-    pos_1 = robot.getDevice("left wheel sensor")
-    pos_2 = robot.getDevice("right wheel sensor")
+    pos_1 = robot.getDevice("left_wheel_sensor")
+    pos_2 = robot.getDevice("right_wheel_sensor")
     # pos_3 = robot.getDevice("pos_3")  
 
     # enable position sensors
     pos_1.enable(time_step)
     pos_2.enable(time_step)
     # pos_3.enable(time_step)
-    print("Wheel Sensors ", pos_1, pos_2)
+    # print("Wheel Sensors ", pos_1, pos_2)
 
     # define infra red sensors
     ir_1 = robot.getDevice("ps0")
@@ -130,13 +138,13 @@ def init_robot(time_step=32):
     ir_7.enable(time_step)
     ir_8.enable(time_step)
 
-    print("IR Sensors ", ir_1, ir_2, ir_3, ir_4, ir_5, ir_6, ir_7, ir_8)
+    # print("IR Sensors ", ir_1, ir_2, ir_3, ir_4, ir_5, ir_6, ir_7, ir_8)
 
 
     wheel_cirum = 2 * math.pi * WHEEL_RADIUS
     encoder_unit = wheel_cirum / (2*math.pi)
-    print("Wheel Circumference: ", wheel_cirum)
-    print("Encoder Unit: ", encoder_unit)
+    # print("Wheel Circumference: ", wheel_cirum)
+    # print("Encoder Unit: ", encoder_unit)
 
     # define and enable lidar sensors
     # lidar = robot.getDevice("head_hokuyo_sensor");
@@ -145,47 +153,56 @@ def init_robot(time_step=32):
     # define and enable gps
     gps = robot.getDevice("gps")
     gps.enable(time_step)
-    print("GPS Sensor ", gps)
+    # print("GPS Sensor ", gps)
 
     # define and enable compass
     compass = robot.getDevice("compass")
     compass.enable(time_step)
-    print("Compass Sensor ", compass)
+    # print("Compass Sensor ", compass)
+
+    imu = robot.getDevice("inertial_unit")
+    imu.enable(time_step)
 
     print("Initializing...")
 
-    return robot
+    return robot, goal_pos, start_pos
+
 
 def read_sensors_values():
 
     global gps_values
     global compass_val
-    global sonar_value
     global position_value
     global ir_value
+    global imu_yaw
 
     # read GPS values
     gps_values = gps.getValues()
-    #print("GPS Read: ", gps_values[0], gps_values[1], gps_values[2])
+    # print("GPS Read: ", gps_values[0], gps_values[1], gps_values[2])
 
     # read compass and rotate arrow accordingly
     compass_val = compass.getValues()
-    #print("Compass Read: ", compass_val)
-
-    # read sonar sensors values
-    sonar_value = np.array([sonar_3.getValue(),sonar_1.getValue(),sonar_2.getValue()])
-    #print("Sonar Read: ", sonar_value)
+    # print("Compass Read: ", compass_val)
 
     # read position sensors values
     position_value = encoder_unit*np.array([pos_1.getValue(),pos_2.getValue()])
-    #print("Position Read: ", position_value)
+    # print("Position Read: ", position_value)
     
     # read infra-red sensors values
     ir_value = np.array([ir_1.getValue(),ir_2.getValue(), ir_3.getValue(), ir_4.getValue(), ir_5.getValue(), ir_6.getValue(), ir_7.getValue(), ir_8.getValue()])
-    #print("IR Read: ", ir_value)
+    # print("IR Read: ", ir_value)
 
-    #print("Sensor Read Complete")
-    return gps_values,compass_val,sonar_value,position_value,ir_value
+    # read the z-axis rotation from the yaw value
+    imu_rpy = imu.getRollPitchYaw()
+    imu_yaw = math.degrees(imu_rpy[2])
+    if(imu_yaw < 0):
+       imu_yaw += 360
+    elif(imu_yaw > 360):
+       imu_yaw -= 360
+    # print("Orientation: ", imu_yaw)
+
+    # print("Sensor Read Complete")
+    return gps_values,compass_val,position_value,ir_value, imu_yaw
 
 def init_robot_state(in_pos=robot_position,in_omega=robot_omega):
     global robot_position
@@ -204,24 +221,26 @@ def update_robot_state():
     # updating the currnet robot position
     robot_position[0] = gps_values[0]
     robot_position[1] = gps_values[1]
-    print("Robot Position Compass: ", robot_position[2])
-    print("Robot Position [1]: ", robot_position[1])
-    print("Robot Position [0]: ", robot_position[0])
+    # print("Robot Position Compass: ", robot_position[2])
+    # print("Robot Position [1]: ", robot_position[1])
+    # print("Robot Position [0]: ", robot_position[0])
 
 def update_motor_speed(input_omega=robot_omega):
     motor_1.setVelocity(input_omega[0])
     motor_2.setVelocity(input_omega[1])
     # motor_3.setVelocity(input_omega[2])
-    print("Motor 1 Speed Updated to: ", motor_1.getVelocity)
-    print("Motor 2 Speed Updated to: ", motor_2.getVelocity)
+    # print("Motor 1 Speed Updated to: ", input_omega[0])
+    # print("Motor 2 Speed Updated to: ", input_omega[1])
 
-def get_bearing_in_degrees(north):
+def get_north_bearing_in_degrees(north):
   rad = math.atan2(north[0], north[1])
   #print(rad)
-  bearing = (rad - 1.5708) / 3.14 * 180.0
+  bearing = math.degrees(rad)
   #print(bearing)
   if (bearing < 0.0):
     bearing = bearing + 360.0
-  print("bearing: ", bearing)
+  elif (bearing > 360):
+     bearing = bearing - 360
+  # print("bearing: ", bearing)
   
   return bearing
